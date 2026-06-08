21a40a6ba550ab065c55ddf6034f2597373b8924
khalednasr <k.nasr92@gmail.com>
2026-06-08 12:06:13 +0200

    removed vomnibar shortcuts

--- modules/features/vimium.nix
+++ modules/features/vimium.nix
@@ -13,23 +13,8 @@
         "map <c-u> scrollPageUp"
         "map G scrollToBottom"
         "map gg scrollToTop"
-        "map o Vomnibar.activate"
-        "map b Vomnibar.activate"
-        "map O Vomnibar.activateInNewTab"
-        "map B Vomnibar.activateBookmarksInNewTab"
         "map f LinkHints.activateMode"
         "map F LinkHints.activateModeToOpenInNewTab"
-      ];
-
-      searchEngines = lib.concatStringsSep "\\n" [
-        "w: https://www.wikipedia.org/w/index.php?title=Special:Search&search=%s Wikipedia"
-        "d: https://www.duckduckgo.com/search?q=%s DuckDuckGo"
-        "g: https://www.google.com/search?q=%s Google"
-        "y: https://www.youtube.com/results?search_query=%s Youtube"
-        "gm: https://www.google.com/maps?q=%s Google Maps"
-        "nxp: https://search.nixos.org/packages?channel=unstable&query=%s NixOS Packages"
-        "nxo: https://search.nixos.org/options?channel=unstable&query=%s NixOS Options"
-        "hmo: https://home-manager-options.extranix.com/?query=%s&release=master Home Manager Options"
       ];
     in
     {
@@ -38,7 +23,6 @@
           {
             "keyMappings": "${keyMappings}",
             "grabBackFocus": true,
-            "searchEngines": "${searchEngines}",
             "settingsVersion": "2.3.1",
             "exclusionRules": [
               {
